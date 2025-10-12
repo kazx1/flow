08b04c58a4f405126e811569b4259768cda06577
Kaz <163393270+kazx1@users.noreply.github.com>
2025-10-12 15:34:59 +0200

    Delete daystart.py

--- daystart.py
+++
@@ -1,9 +0,0 @@
-
-import os, webbrowser
-
-os.startfile("C:/Program Files/Notepad++/notepad++.exe")
-
-webbrowser.open("https://mail.google.com")
-webbrowser.open("https://calendar.google.com")
-
-print("✅ Your work setup is ready!")
